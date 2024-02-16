2cbaa1fd105c4da60d9318af7adf6e653f5b0332
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2024-02-16 16:50:22 -0500

    standing on the shoulders of giants is diminutive to the heights they've lifted me

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ I want to make the light sing
 The man, the legend put together great modules to read rotary encoders
 Standing on the shoulders of giants and shitting bricks the whole time man
 -https://github.com/miketeachman/micropython-rotary
+
+## Hella shout out to Pimironi and VEEB
+1. Take the pirate MicroPython and load it into your pico
+1. Load in Teachman's awesome libraries
+1. Load up my wet bag of shit
+1. Rejoice
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import time
 from rotary_irq_rp2 import RotaryIRQ
+from machine import Pin, SoftI2C
+import machine
+import ustruct
+import pimoroni_i2c
+import breakout_bh1745
+import utime
+import sys
+import math
+import gc
 
+led = Pin("LED", Pin.OUT)
 r = RotaryIRQ(pin_num_clk=12, 
               pin_num_dt=13, 
               min_val=0, 
@@ -9,11 +19,39 @@ r = RotaryIRQ(pin_num_clk=12,
               range_mode=RotaryIRQ.RANGE_WRAP)
               
 val_old = r.value()
-while True:
+
+
+
+try:
+    LIGHTSENSOR = {"sda": 0, "scl": 1}
+    I2C = pimoroni_i2c.PimoroniI2C(**LIGHTSENSOR)
+    bh1745 = breakout_bh1745.BreakoutBH1745(I2C)
+    bh1745.leds(False)
+except:
+    print("Check the sensor. An exception occurred") # A visual cue that therehas been an issue with the sensor setup
+    print("Sensor data:", sensor_data)
+    
+    # read the value from the light sensor
+def sensorread():
+    rgbc_raw = bh1745.rgbc_raw()
+    rgb_clamped = bh1745.rgbc_clamped()
+    brightness=rgbc_raw[3]
+    print("Clamped: {}, {}, {}, {}".format(*rgb_clamped))
+    print("Bright="+str(brightness))
+    try:
+        EV = math.log2(brightness/calibrationconst)+evcorrection
+        print(EV)
+    except:
+        EV = -10
+    return rgb_clamped[0],rgb_clamped[1],rgb_clamped[2],EV
+
+while(1):
+    red, green, blue, lastmeasure=sensorread()
+    print(red,green,blue,lastmeasure)
+    led.value(0)
     val_new = r.value()
     
     if val_old != val_new:
         val_old = val_new
         print('result =', val_new)
-        
     time.sleep_ms(50)
